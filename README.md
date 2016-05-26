4091c0b93c47f206ee58834231c3ac8502a071a4
Pavel Tetyaev <pahanini@gmail.com>
2016-05-26 13:10:49 +0500

    First version

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# logrus-logstash-hook
-Golang logrus logstash hook
+# Logstash hook for logrus <img src="http://i.imgur.com/hTeVwmJ.png" width="40" height="40" alt=":walrus:" class="emoji" title=":walrus:" />
+
+Based on [this](https://github.com/bshuster-repo/logrus-logstash-hook) hook. But instead of using message context
+use global 'type' filed for logging
+
+## Usage
+
+```go
+package main
+
+import (
+        "github.com/Sirupsen/logrus"
+        "github.com/sima-land/logrus-logstash-hook"
+)
+
+func main() {
+        log := logrus.New()
+        hook, err := logrus_logstash.NewHook("tcp", "172.17.0.2:9999", "myApp")
+        if err != nil {
+                log.Fatal(err)
+        }
+        log.Hooks.Add(hook)
+        log.Info("Hello") // <-- type "myApp" will be added here for logstash
+}
+```
+
---
+++ hook.go
@@ -0,0 +1,62 @@
+package logrus_logstash
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/Sirupsen/logrus"
+	"net"
+)
+
+// Hook represents a connection to a Logstash instance
+type Hook struct {
+	conn     net.Conn
+	typeName string
+}
+
+// NewHook creates a new hook to a Logstash instance, which listens on
+// `protocol`://`address`.
+func NewHook(protocol, address, typeName string) (*Hook, error) {
+	conn, err := net.Dial(protocol, address)
+	if err != nil {
+		return nil, err
+	}
+	return &Hook{conn: conn, typeName: typeName}, nil
+}
+
+func (h *Hook) format(entry *logrus.Entry) ([]byte, error) {
+	fields := make(logrus.Fields)
+	for k, v := range entry.Data {
+		fields[k] = v
+	}
+	fields["@timestamp"] = entry.Time.Format(logrus.DefaultTimestampFormat)
+	fields["message"] = entry.Message
+	fields["level"] = entry.Level.String()
+	fields["type"] = h.typeName
+	serialized, err := json.Marshal(fields)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to marshal fields to JSON, %v", err)
+	}
+	return append(serialized, '\n'), nil
+}
+
+func (h *Hook) Fire(entry *logrus.Entry) error {
+	dataBytes, err := h.format(entry)
+	if err != nil {
+		return err
+	}
+	if _, err = h.conn.Write(dataBytes); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (h *Hook) Levels() []logrus.Level {
+	return []logrus.Level{
+		logrus.PanicLevel,
+		logrus.FatalLevel,
+		logrus.ErrorLevel,
+		logrus.WarnLevel,
+		logrus.InfoLevel,
+		logrus.DebugLevel,
+	}
+}
